5758bb2bea50d7844cbc9db929be8fe642fdda5a
Alexis Torchio <torchio.alexis@gmail.com>
2021-03-26 12:01:45 -0300

    Completados los ejemplos de trabajo con Promise antes del challenge

--- src/promise/index.js
+++ src/promise/index.js
@@ -12,3 +12,29 @@ const somethingWillHappen = () => {
 somethingWillHappen ()
     .then(response => console.log (response))
     .catch(err => console.error(err));
+
+const somethingWillHappen2 = () => {
+    return new Promise ((resolve, reject) => {
+        if (true) {
+            setTimeout (() => {
+                resolve ('Otro Heeeey');
+            }, 2000)
+        }
+        else {
+            const error = new Error ('Otro Whoops!');
+            reject (error);
+        }
+    });
+}
+
+somethingWillHappen2 ()
+    .then (response => console.log (response))
+    .catch (err => console.error(err));
+
+Promise.all ([somethingWillHappen (), somethingWillHappen2 ()])
+    .then (response => {
+        console.log ('Array of results', response);
+    })
+    .catch (err => {
+        console.error(err);
+    })
